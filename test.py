cb806aaaa3d787d3db4c977974e7f170acd70f71
jlztorres <jl.zelayat@alum.up.edu.pe>
2023-02-28 09:26:32 -0500

    primeras mods

--- test.py
+++ test.py
@@ -19,27 +19,3 @@ rotated_rectangle.reset_index(drop=True, inplace=True)
 print('Rectángulo rotado:')
 print(rotated_rectangle)
 
-# Crear un GeoSeries con un punto
-point = gpd.GeoSeries(Point(2, 1.5))
-
-# Crear una figura y unos ejes
-fig, ax = plt.subplots()
-
-# Graficar el rectángulo original
-rectangle.plot(ax=ax, color='blue', alpha=0.1, edgecolor='none')
-
-# Graficar el rectángulo rotado
-rotated_rectangle.plot(ax=ax, color='red', alpha=0.1, edgecolor='none')
-
-# Verificar si el punto se encuentra dentro del rectángulo rotado
-if rotated_rectangle.contains(point.iloc[0]):
-    print('El punto se encuentra dentro del rectángulo rotado.')
-    point.plot(ax=ax, color='green', markersize=100)
-else:
-    print('El punto no se encuentra dentro del rectángulo rotado.')
-    point.plot(ax=ax, color='red', markersize=100)
-
-# Configurar la figura y mostrarla
-ax.set_title('Rectángulo original y rectángulo rotado')
-ax.set_aspect('equal')
-plt.show()
